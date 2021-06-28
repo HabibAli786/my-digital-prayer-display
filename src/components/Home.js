287ec6b041533897b541f0e84abc0f2dd4ff1dec
Habib <habibali6.ha@gmail.com>
2021-06-28 23:55:05 +0100

    basic title and subheading created

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import { Navbar, Container, Nav, Button, NavDropdown } from 'react-bootstrap'
+import { Navbar, Container, Nav, Button, Row, Col } from 'react-bootstrap'
 import { BsClockHistory } from 'react-icons/bs'
 import './Home.css'
 
@@ -6,6 +6,7 @@ function Home() {
 
     return(
         <>
+        {/* NavBar Section */}
         <Navbar collapseOnSelect expand="lg" bg="light">
         <Container>
             <Navbar.Brand href="/">
@@ -24,6 +25,25 @@ function Home() {
             </Navbar.Collapse>
         </Container>
         </Navbar>
+
+        {/* Main Content Area */}
+        <Container className="main-container">
+        <Row className="main-row-title">
+            <Col className="main-col">
+                <h1 className="header">Welcome to PrayerTimes+</h1>
+            </Col>
+        </Row>
+        <Row className="main-row-title mt-5">
+            <Col>
+                <p className="subheading">A lightweight application to display prayer times for your masjid.</p>
+            </Col>
+        </Row>
+        {/* <Row className="main-row">
+            <Col className="main-col">
+                <h1>Welcome to PrayerTimes+</h1>
+            </Col>
+        </Row> */}
+        </Container>
         </>
     )
 }
