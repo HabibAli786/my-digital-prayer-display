14adba3099db0693ab418f688c81fd183ca437b4
Habib <habibali6.ha@gmail.com>
2021-06-30 23:22:33 +0100

    changes to table

--- src/components/prayerTimes/prayerTimes.js
+++ src/components/prayerTimes/prayerTimes.js
@@ -10,43 +10,43 @@ function PrayerTimes() {
                 <Col className="col-3"></Col>
                 <Col className="col-3"></Col>
                 <Col className="col-3">Start Time</Col>
-                <Col className="col-3">Jamaat</Col>
+                <Col className="col-3 active-color">Jamaat</Col>
             </Row>
             <Row>
                 <Col className="col-3">فَجْر‎</Col>
                 <Col className="col-3">Fajr</Col>
                 <Col className="col-3">3:06</Col>
-                <Col className="col-3">4:00</Col>
+                <Col className="col-3 active-color">4:00</Col>
             </Row>
             <Row>
                 <Col className="col-3">Don't know</Col>
                 <Col className="col-3">4:41</Col>
                 <Col className="col-3">-- --</Col>
-                <Col className="col-3">-- --</Col>
+                <Col className="col-3 active-color">-- --</Col>
             </Row>
             <Row>
                 <Col className="col-3">صَلَاة ٱلظُّهْر</Col>
                 <Col className="col-3">Zuhr</Col>
                 <Col className="col-3">1:11</Col>
-                <Col className="col-3">1:30</Col>
+                <Col className="col-3 active-color">1:30</Col>
             </Row>
             <Row>
                 <Col className="col-3">صَلَاةُ العَصْر</Col>
                 <Col className="col-3">Asr</Col>
                 <Col className="col-3">6:44</Col>
-                <Col className="col-3">8:00</Col>
+                <Col className="col-3 active-color">8:00</Col>
             </Row>
             <Row>
                 <Col className="col-3">صَلَاةُ اَلْمَغْرِب</Col>
                 <Col className="col-3">Maghrib</Col>
                 <Col className="col-3">9:32</Col>
-                <Col className="col-3">9:32</Col>
+                <Col className="col-3 active-color">9:32</Col>
             </Row>
             <Row>
                 <Col className="col-3">صَلَاةُ العِشَاء‎</Col>
                 <Col className="col-3">Isha</Col>
                 <Col className="col-3">10:50</Col>
-                <Col className="col-3">11:00</Col>
+                <Col className="col-3 active-color">11:00</Col>
             </Row>
         </Container>
         </>
