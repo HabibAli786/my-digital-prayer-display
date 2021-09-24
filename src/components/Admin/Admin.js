a3433257decf5d8ab616ea498e973d8f80c0443a
Habib <habibali6.ha@gmail.com>
2021-09-24 16:43:59 +0100

    layout created for admin page

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -8,44 +8,41 @@ function Admin() {
     return (
         <>
         <Header />
-        <div className="main-content">
+        {/* <div className="main-content-admin"> */}
 
-            <Container className="main-container">
+            <Container className="image-container mt-5">
                 <Row>
                     <Col>
                         <img className="admin-icon" src="images/admin-icon.png" alt="admin icon" />
                     </Col>
                 </Row>
+            </Container>
 
-                <Row>
+            <Container className="login-container" >
+                <Row className="login-row">
                     <Col>
-                        <h1>Admin Login</h1>                
+                        <h1 className="login-header">Admin Login</h1>                
                     </Col>
                 </Row>
 
                 <Row>
-                    <Form>
-                        <Form.Group className="mb-3 input-field" controlId="formBasicEmail">
-                            {/* <Form.Label>Email address</Form.Label> */}
-                            <Form.Control placeholder="Username..." />
-                            {/* <Form.Text className="text-muted">
-                            We'll never share your email with anyone else.
-                            </Form.Text> */}
+                    <Form className="input-lg">
+                        <Form.Group className="mb-5 input-field">
+                            <Form.Control style={{fontSize : "40px" }} placeholder="Username..." />
                         </Form.Group>
 
-                        <Form.Group className="mb-3 input field" controlId="formBasicPassword">
-                            <Form.Control type="password" placeholder="Password..." />
+                        <Form.Group className="mb-5 input-field" controlId="formBasicPassword">
+                            <Form.Control style={{fontSize : "40px" }} type="password" placeholder="Password..." />
                         </Form.Group>
 
-                        <Button variant="primary" type="submit">
+                        <Button style={{fontSize : "40px", borderRadius: "10px", width: "20rem" }} variant="primary" type="submit">
                             Submit
                         </Button>
                     </Form>
                 </Row>
-
             </Container>
 
-        </div>
+        {/* </div> */}
         </>
     )
 
