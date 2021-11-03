9240460b1b8ee60912b8a612429771c7d4f4f446
Habib <habibali6.ha@gmail.com>
2021-11-03 18:41:36 +0000

    file upload to state through form

--- src/components/UploadTimetable/UploadTimetable.js
+++ src/components/UploadTimetable/UploadTimetable.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 import { useEffect, useState } from "react"
+import { Button, Col, Container, Form, Row } from "react-bootstrap";
 import { connect } from "react-redux";
 import { Redirect } from 'react-router';
+import { AiOutlineCloudUpload } from "react-icons/ai";
+
+import Header from '../Header/Header'
+import './UploadTimetable.css'
 import { set_auth, set_username } from "../Redux/actions/authAction";
 import { authenticate } from "../Redux/reducers/authReducer";
 
@@ -9,17 +14,45 @@ function UploadTimetable(props) {
 
     const { auth, set_auth, username } = props
 
+    const [file, setFile] = useState(null)
+
+    const uploadFile = (e) => {
+        e.preventDefault()
+        // console.log(e.target.value)
+        console.log(file)
+    }
+
     useEffect(() => {
         authenticate()
     }, [])
 
-    console.log(auth)
-    
     if(auth === "Unsuccessfully Authenticated" || auth === "Server Offline" ) {
         return ( <Redirect to="/admin" /> )
     } else {
         return (
-            <h1>UploadTimetable</h1>
+            <>
+            <Header />
+            <h1 style={{marginTop : "15px", fontSize: "50px"}}>UploadTimetable</h1>
+            <Container className="uploadTimetable-container">
+                <Row>
+                    <Col>
+                        <AiOutlineCloudUpload size={200} />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <Form onSubmit={uploadFile}>
+                            <input 
+                                style={{ marginTop : "100px", fontSize: "30px", borderStyle: "solid" }} 
+                                type="file"
+                                onChange={(e) => setFile(e.target.files[0]) } />
+                            <br />
+                            <Button type="submit" style={{ marginTop : "15px" }}>Upload Timetable</Button>
+                        </Form>
+                    </Col>
+                </Row>
+            </Container>
+            </>
         )
     }
 }
