9a4ed02c4092689603efc34351193aeadd07cdd1
Habib <habibali6.ha@gmail.com>
2021-11-11 16:16:52 +0000

    starting structure

--- src/components/EditTimetable/EditTimetable.js
+++ src/components/EditTimetable/EditTimetable.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useEffect, useState } from "react"
 import { connect } from "react-redux";
 import { Redirect } from 'react-router';
+import Header from "../Header/Header";
 import { set_auth, set_username } from "../Redux/actions/authAction";
 import { authenticate } from "../Redux/reducers/authReducer";
 
@@ -19,7 +20,10 @@ function EditTimetable(props) {
         return ( <Redirect to="/admin" /> )
     } else {
         return (
-            <h1>Edit Timetable</h1>
+            <>
+            <Header />
+            <h1 style={{marginTop : "15px", fontSize: "50px"}}>Edit Timetable</h1>
+            </>
         )
     }
 }
