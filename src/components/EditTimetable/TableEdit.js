c6db77859cbac4c9dd3481e1bf9ff942054195f9
Habib <habibali6.ha@gmail.com>
2021-11-17 11:26:44 +0000

    edit table component

---
+++ src/components/EditTimetable/TableEdit.js
@@ -0,0 +1,86 @@
+import React from 'react'
+import { Table } from 'react-bootstrap'
+import { useTable, usePagination } from 'react-table'
+
+import './EditTimetable'
+
+// Create an editable cell renderer
+const EditableCell = ({
+  value: initialValue,
+  row: { index },
+  column: { id },
+  updateMyData, // This is a custom function that we supplied to our table instance
+}) => {
+  // We need to keep and update the state of the cell normally
+  const [value, setValue] = React.useState(initialValue)
+
+  const onChange = e => {
+    setValue(e.target.value)
+  }
+
+  // We'll only update the external data when the input is blurred
+  const onBlur = () => {
+    updateMyData(index, id, value)
+  }
+
+  // If the initialValue is changed external, sync it up with our state
+  React.useEffect(() => {
+    setValue(initialValue)
+  }, [initialValue])
+
+  return <input value={value} onChange={onChange} onBlur={onBlur} />
+}
+
+// Set our editable cell renderer as the default Cell renderer
+const defaultColumn = {
+  Cell: EditableCell,
+}
+
+// Be sure to pass our updateMyData and the skipPageReset option
+function TableEdit({ columns, data, updateMyData, skipPageReset }) {
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    prepareRow,
+    rows,
+  } = useTable(
+    {
+      columns,
+      data,
+      defaultColumn,
+      updateMyData,
+    }
+  )
+
+  // Render the UI for your table
+  return (
+    <>
+      <Table className="edit_table" {...getTableProps()} striped bordered hover>
+        <thead>
+          {headerGroups.map(headerGroup => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map(column => (
+                <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row, i) => {
+            prepareRow(row)
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map(cell => {
+                  return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                })}
+              </tr>
+            )
+          })}
+        </tbody>
+      </Table>
+    </>
+  )
+}
+
+export default TableEdit
