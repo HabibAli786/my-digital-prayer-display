1c6f440df475af775f6af418b287f877ba6638fb
Habib <habibali6.ha@gmail.com>
2021-11-24 16:00:10 +0000

    dispatch added for auth on EditNotifications

--- src/components/EditNotifications/EditNotifications.js
+++ src/components/EditNotifications/EditNotifications.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useEffect, useState, useMemo } from "react"
 import { Button, Card, Col, Container, Form, Row } from "react-bootstrap";
-import { connect } from "react-redux";
+import { connect, useDispatch } from "react-redux";
 import { Redirect } from 'react-router';
 import { v4 as uuidv4 } from 'uuid';
 
@@ -12,6 +12,7 @@ import './EditNotifications.css'
 
 function EditNotifications(props) {
 
+    const dispatch = useDispatch()
     const { auth } = props
 
     const [notifications, setNotifications] = useState([])
@@ -46,7 +47,7 @@ function EditNotifications(props) {
     }
 
     useEffect(() => {
-        authenticate()
+        dispatch(authenticate())
         getNotifications()
     }, [notifications.length])
 
