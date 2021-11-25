517221e7ef8f97205ed05ed4e8b3a17e7f7fb6bd
Habib <habibali6.ha@gmail.com>
2021-11-25 16:51:59 +0000

    add feature working for editNotifications

--- src/components/EditNotifications/EditNotifications.js
+++ src/components/EditNotifications/EditNotifications.js
@@ -46,6 +46,25 @@ function EditNotifications(props) {
         })
     }
 
+    const addNotification = (event) => {
+        // console.log(event)
+        event.preventDefault()
+        const data  = event.target.notification.value
+        axios({
+            method: 'POST',
+            data: {
+                toAdd : data
+            },
+            withCredentials: true,
+            url: 'http://localhost:3001/notifications/add'
+        })
+        .then((res) => {
+            console.log(res.data)
+            getNotifications()
+            event.target.reset()
+        })
+    }
+
     useEffect(() => {
         dispatch(authenticate())
         getNotifications()
@@ -64,7 +83,7 @@ function EditNotifications(props) {
                 <Row className="notification-header-row">
                         <h1 style={{textAlign: "left", fontSize: "45px"}}>List of Notifications</h1>
                 </Row>
-                <div style={{overflowY: "scroll", overflowX: "hidden", height: "500px"}}>
+                <div style={{overflowY: "scroll", overflowX: "hidden", height: "480px"}}>
                     {notifications ? notifications.map(
                         notification => 
                         <Row key={uuidv4()} className="notification-row">
@@ -88,17 +107,17 @@ function EditNotifications(props) {
                 <Row className="new-notification-header-row">
                     <h1 style={{textAlign: "left", fontSize: "45px"}}>New Notifications</h1>
                 </Row>
-                <Row className="notification-row">
+                <Row className="new-notification-row">
+                <Form onSubmit={addNotification}>
                     <Col className="notifications" lg={10}>
-                    <Form>
-                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                            <Form.Control style={{fontSize: "45px"}} type="text" placeholder="Enter New Notification Here" />
+                        <Form.Group className="mb-3">
+                            <Form.Control name="notification" style={{fontSize: "45px"}} type="text" placeholder="Enter New Notification Here" />
                         </Form.Group>
-                    </Form>
                     </Col>
                     <Col lg={2}>
-                        <Button variant="primary" className="notification-button">Add</Button>
+                        <Button variant="primary" type="submit" className="notification-button">Add</Button>
                     </Col>
+                </Form>
                 </Row>
             </Container>
             </>
