7b5fdcc8a1b1e0f3fc7e05f652a4177833d187d4
Habib <habibali6.ha@gmail.com>
2022-03-20 05:22:54 +0000

    basic css added

--- src/components/QRCode/QRCode.js
+++ src/components/QRCode/QRCode.js
@@ -1,9 +1,11 @@
+import axios from "axios";
 import { useState } from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import { Col, Container, Row, Button, Form } from "react-bootstrap";
+import { AiOutlineCloudUpload } from "react-icons/ai";
 import Switch from "react-switch";
 import Header from "../Header/Header";
 
-
+import './QRCode.css'
 
 function QRCode() {
 
@@ -13,12 +15,35 @@ function QRCode() {
         setChecked(nextChecked)
     }
 
+    const [file, setFile] = useState(null)
+    const [serverStatus, setServerStatus] = useState(null)
+
+    const uploadFile = (e) => {
+        e.preventDefault()
+        const data = new FormData()
+        data.append('logo', file)
+        // console.log(e.target.value)
+        console.log(file)
+        console.log(data)
+        if(file) {
+            axios.post('http://localhost:3001/media/logo', data, {
+                'content-type': 'multipart/form-data'
+            }).then(res => { // then print response status
+                console.log(res);
+                setServerStatus(res.data)
+            })
+        } else {
+            setServerStatus("Error: No file has been selected")
+        }
+        e.target.logo.value = ""
+    }
+
 
     return (
         <>
         <Header />
         <h1 style={{marginTop : "15px", fontSize: "50px"}}>QR Code Settings</h1>
-        <Container>
+        <Container className="qr-code-container">
             <Row>
                 <Col>
                     <Switch onChange={handleChange} checked={checked} />
@@ -29,6 +54,24 @@ function QRCode() {
                     <h1>QR Code Status: {checked ? "On" : "Off"}</h1>
                 </Col>
             </Row>
+            <Row>
+                <Col>
+                    <AiOutlineCloudUpload size={200} />
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <Form onSubmit={uploadFile}>
+                        <input 
+                            className="uploadQR-inputImage" 
+                            name="logo"
+                            type="file"
+                            onChange={(e) => setFile(e.target.files[0]) } />
+                        <br />
+                        <Button type="submit" style={{ marginTop : "15px" }}>Upload QR Code</Button>
+                    </Form>
+                </Col>
+                </Row>
         </Container>
         </>
     )
