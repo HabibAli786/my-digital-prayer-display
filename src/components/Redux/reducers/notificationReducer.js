c268e0f64a4835b60c560c6ea34b6c70fa8946ef
Habib <habibali6.ha@gmail.com>
2022-04-02 13:52:35 +0100

    thunk removed

--- src/components/Redux/reducers/notificationReducer.js
+++ src/components/Redux/reducers/notificationReducer.js
@@ -24,25 +24,4 @@ const notificationReducer = (state = init, action) => {
     }
 }
 
-export const setNotifi = () => async (dispatch, getState) => {
-    let source = axios.CancelToken.source();
-    // console.log("I am running in thunk")
-    let notifications = null
-
-    axios.get(`http://localhost:3001/notifications`)
-        .then((response) => {
-            // console.log(response.data.notifications)
-            if(response.data.notifications) {
-                notifications = response.data.notifications
-                // console.log(notifications)
-                dispatch(set_notifi(notifications))
-            }
-        })
-        .catch((error) => {
-            console.log(error)
-            notifications = ["No Notifications"]
-    })
-    source.cancel("Cancelling in cleanup");
-}
-
 export default notificationReducer;
